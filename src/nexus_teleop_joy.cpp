68dde0b8008702b1ebdb6e629e3a682b830304ec
sesutton93 <sesutton93@outlook.com>
2022-08-02 17:41:02 +0200

    switched to xbox one controller

--- src/nexus_teleop_joy.cpp
+++ src/nexus_teleop_joy.cpp
@@ -50,8 +50,8 @@ private:
   ros::ServiceClient emergency_cli;
   ros::ServiceClient arming_cli;
   int i_velLinear , i_velAngular;
-  const float max_vel_x = 1.0; // [m/s]
-  const float max_vel_y = 1.0; // [m/s]
+  const float max_vel_x = 100.0; // [m/s]
+  const float max_vel_y = 100.0; // [m/s]
   const float max_vel_th = M_PI_4; // quarter pi [rad/sec]
   bool armed = true;
 };
@@ -78,9 +78,10 @@ void TeleopJoy::callBack(const sensor_msgs::Joy::ConstPtr& joy)
 	nexus_base_ros::ArmingEnable srv2;
 	
 	if(armed) {
-		vel.linear.x = max_vel_x*joy->axes[3]; // right handle up/down
-		vel.linear.y = -max_vel_y*joy->axes[2]; // right handle left-to-right
-		vel.angular.z = -max_vel_th*joy->axes[0]; // left handle left-to-right
+		vel.linear.x = max_vel_x*joy->axes[0]; // right handle up/down
+		vel.linear.y = -max_vel_y*joy->axes[1]; // right handle left-to-right
+		vel.angular.z = max_vel_th*joy->axes[3]; // left handle left-to-right
+
 	}
 	else {
 		vel.linear.x = 0;
@@ -89,11 +90,7 @@ void TeleopJoy::callBack(const sensor_msgs::Joy::ConstPtr& joy)
 	}
 
 	if(joy->buttons[0]){
-		ROS_INFO("Blue button pressed.\n");
-	}
-
-	if(joy->buttons[1]){
-		ROS_INFO("Green button pressed: ");
+		ROS_INFO("A button pressed: ");
 		srv2.request.enable = true;
 		if (arming_cli.call(srv2)) {
 			armed = true;
@@ -106,8 +103,8 @@ void TeleopJoy::callBack(const sensor_msgs::Joy::ConstPtr& joy)
 
 	}
 
-	if(joy->buttons[2]){
-		ROS_INFO("Red button pressed: ");
+	if(joy->buttons[1]){
+		ROS_INFO("B button pressed: ");
 		srv1.request.enable = true;
 		if (emergency_cli.call(srv1)) {
 			armed = false;
@@ -117,6 +114,10 @@ void TeleopJoy::callBack(const sensor_msgs::Joy::ConstPtr& joy)
 			armed = false;
 			ROS_ERROR("Failed to call the service Emergency Stop!\n");
 		}
+	}
+
+	if(joy->buttons[2]){
+		ROS_INFO("X button pressed.\n");
 	}
 
 	if(joy->buttons[3]){
@@ -132,19 +133,18 @@ void TeleopJoy::callBack(const sensor_msgs::Joy::ConstPtr& joy)
 	}
 
 	if(joy->buttons[6]){
-		ROS_INFO("LT pressed.\n");
-	}
-
-	if(joy->buttons[7]){
-		ROS_INFO("RT pressed.\n");
-	}
-
-	if(joy->buttons[8]){
 		ROS_INFO("Back button pressed.\n");
 	}
 
-	if(joy->buttons[9]){
+	if(joy->buttons[7]){
 		ROS_INFO("Start button pressed.\n");
+	}
+	if(joy->buttons[8]){
+		ROS_INFO("LSB button pressed.\n");
+	}
+
+	if(joy->buttons[9]){
+		ROS_INFO("RSB button pressed.\n");
 	}
 
 	pub.publish(vel);
